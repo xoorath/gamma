6b12c189c81d1285d597ff2d26b65170fc9f9239
xoorath <jared@xoorath.com>
2016-06-27 22:31:40 -0400

    basic formatting

--- Gamma/Src/Main.cpp
+++ Gamma/Src/Main.cpp
@@ -18,82 +18,82 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int main()
 {
-    HINSTANCE hInstance = ::GetModuleHandle(NULL);
+  HINSTANCE hInstance = ::GetModuleHandle(NULL);
 
-    WNDCLASSEX wcex;
+  WNDCLASSEX wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = szWindowClass;
-    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+  wcex.cbSize = sizeof(WNDCLASSEX);
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+  wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = NULL;
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
 
-    if (!RegisterClassEx(&wcex))
-    {
-        MessageBox(NULL,
-            _T("Call to RegisterClassEx failed!"),
-            _T("Win32 Guided Tour"),
-            NULL);
+  if (!RegisterClassEx(&wcex))
+  {
+    MessageBox(NULL,
+               _T("Call to RegisterClassEx failed!"),
+               _T("Win32 Guided Tour"),
+               NULL);
 
-        return 1;
-    }
+    return 1;
+  }
 
-    hInst = hInstance; // Store instance handle in our global variable
+  hInst = hInstance; // Store instance handle in our global variable
 
-                       // The parameters to CreateWindow explained:
-                       // szWindowClass: the name of the application
-                       // szTitle: the text that appears in the title bar
-                       // WS_OVERLAPPEDWINDOW: the type of window to create
-                       // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
-                       // 500, 100: initial size (width, length)
-                       // NULL: the parent of this window
-                       // NULL: this application does not have a menu bar
-                       // hInstance: the first parameter from WinMain
-                       // NULL: not used in this application
-    HWND hWnd = CreateWindow(
-        szWindowClass,
-        szTitle,
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        500, 100,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
-        );
+                     // The parameters to CreateWindow explained:
+                     // szWindowClass: the name of the application
+                     // szTitle: the text that appears in the title bar
+                     // WS_OVERLAPPEDWINDOW: the type of window to create
+                     // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+                     // 500, 100: initial size (width, length)
+                     // NULL: the parent of this window
+                     // NULL: this application does not have a menu bar
+                     // hInstance: the first parameter from WinMain
+                     // NULL: not used in this application
+  HWND hWnd = CreateWindow(
+    szWindowClass,
+    szTitle,
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, CW_USEDEFAULT,
+    500, 100,
+    NULL,
+    NULL,
+    hInstance,
+    NULL
+    );
 
-    if (!hWnd)
-    {
-        MessageBox(NULL,
-            _T("Call to CreateWindow failed!"),
-            _T("Win32 Guided Tour"),
-            NULL);
+  if (!hWnd)
+  {
+    MessageBox(NULL,
+               _T("Call to CreateWindow failed!"),
+               _T("Win32 Guided Tour"),
+               NULL);
 
-        return 1;
-    }
+    return 1;
+  }
 
-    // The parameters to ShowWindow explained:
-    // hWnd: the value returned from CreateWindow
-    // nCmdShow: the fourth parameter from WinMain
-    ShowWindow(hWnd, SW_SHOW);
-    UpdateWindow(hWnd);
+  // The parameters to ShowWindow explained:
+  // hWnd: the value returned from CreateWindow
+  // nCmdShow: the fourth parameter from WinMain
+  ShowWindow(hWnd, SW_SHOW);
+  UpdateWindow(hWnd);
 
-    // Main message loop:
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+  // Main message loop:
+  MSG msg;
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
 
-    return (int)msg.wParam;
+  return (int)msg.wParam;
 }
 
 //
@@ -107,32 +107,32 @@ int main()
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    PAINTSTRUCT ps;
-    HDC hdc;
-    TCHAR greeting[] = _T("Hello, World!");
+  PAINTSTRUCT ps;
+  HDC hdc;
+  TCHAR greeting[] = _T("Hello, World!");
 
-    switch (message)
-    {
+  switch (message)
+  {
     case WM_PAINT:
-        hdc = BeginPaint(hWnd, &ps);
+      hdc = BeginPaint(hWnd, &ps);
 
-        // Here your application is laid out.
-        // For this introduction, we just print out "Hello, World!"
-        // in the top left corner.
-        TextOut(hdc,
-            5, 5,
-            greeting, _tcslen(greeting));
-        // End application-specific layout section.
+      // Here your application is laid out.
+      // For this introduction, we just print out "Hello, World!"
+      // in the top left corner.
+      TextOut(hdc,
+              5, 5,
+              greeting, _tcslen(greeting));
+          // End application-specific layout section.
 
-        EndPaint(hWnd, &ps);
-        break;
+      EndPaint(hWnd, &ps);
+      break;
     case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
+      PostQuitMessage(0);
+      break;
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-        break;
-    }
+      return DefWindowProc(hWnd, message, wParam, lParam);
+      break;
+  }
 
-    return 0;
+  return 0;
 }
